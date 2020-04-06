b592aa5d4027697694790c6669504dcf04f0491e
MPLeung <mpltest2020@gmail.com>
2020-04-06 21:46:00 +0800

    update

--- create-bucket.sh
+++ create-bucket.sh
@@ -1,5 +1,5 @@
-#gsutil mb -c standard -l us-east1 gs://$bucket
-gsutil mb -c standard -l southamerica-east1 gs://$bucket
+gsutil mb -c standard -l us-east1 gs://$bucket
+#gsutil mb -c standard -l southamerica-east1 gs://$bucket
 
 gsutil cp sally.jpg gs://$bucket
 gsutil cp index.html gs://$bucket
